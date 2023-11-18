a21a2b98a55e946ecfd5b8713cf85c01c9352cd3
Max <mdjaya@protonmail.com>
2023-11-17 23:57:43 -0500

    started Connect 4

---
+++ ConnectFour/app.js
@@ -0,0 +1,55 @@
+let turn = false;
+let won = false;
+let board = [];
+let heights = [];
+for (let i = 0; i < 6; i++){
+	board.push([]);
+	heights.push(0);
+	for (let j = 0; j < 7; j++){
+		board[i].push(0);
+	}
+}
+heights.push(0);
+
+function play(c){
+	if (heights[c] == board.length || won)
+		return;
+	board[heights[c]][c] = turn ? 1 : 2;
+	const element = document.getElementById("" + (heights[c]*7 + c))
+	element.style.backgroundColor = turn ? "#e03809" : "#dbeb34";
+	won = isWon(heights[c], c);
+	if (won)
+		document.getElementById("winner_card").innerHTML = 
+			turn ? "Red Wins" : "Yellow Wins";
+	heights[c] += 1;
+	turn = !turn;
+}
+
+function isWon(r, c){
+	let lr = directiveSearch(board[r][c], r, c, 0, -1) +
+		directiveSearch(board[r][c], r, c, 0, 1) - 1;
+	let ud = directiveSearch(board[r][c], r, c, -1, 0) + 
+		directiveSearch(board[r][c], r, c, 1, 0) - 1;
+	let rd = directiveSearch(board[r][c], r, c, -1, -1) + 
+		directiveSearch(board[r][c], r, c, 1, 1) - 1;
+	let ld = directiveSearch(board[r][c], r, c, 1, -1) +
+		directiveSearch(board[r][c], r, c, -1, 1) -1;
+	return ld > 3 || rd > 3 || ud > 3 || lr > 3;
+
+}
+
+function directiveSearch(color, r, c, dr, dc){
+	try {
+		if (board[r][c] != color)
+			return 0;
+	} catch(err){
+		return 0;
+	}
+	return 1 + directiveSearch(color, r+dr, c+dc, dr, dc);
+}
+
+
+const cells = document.getElementsByClassName("cell");
+for (let i = 0; i < cells.length; i++){
+	cells[i].addEventListener("click", function(){play(parseInt(cells[i].id)%7);}); 
+}
