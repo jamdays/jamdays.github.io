e275e73060f8973f6ad6ee1b24bd2f37e89e923c
jamdays <venuedollar@protonmail.com>
2024-06-30 02:28:50 -0700

    added icon in corner

--- EightPuzzle/app.js
+++ EightPuzzle/app.js
@@ -1,47 +1,73 @@
-let grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let grids = {};
 let dims = 3;
-const cells = document.getElementsByClassName("cell");
-for (let i = 0; i < cells.length; i++){
-	let k = i;
-	cells[i].addEventListener("click", function(){move(k);});
+let won = {}
+let album_cells = ["lr", "y"];
+let albums = ["late registration", "yeezus"];
+for (let j = 0; j < albums.length; j++){
+	let cells = document.getElementsByClassName(`cell_${album_cells[j]}`);
+	for (let i = 0; i < cells.length; i++){
+		let k = i;
+		let elem = cells[i];
+		elem.addEventListener("click", function(){move(k, albums[j]); checkWon(albums[j]);});
+		elem.style.height = "100px";
+	}
+	grids[albums[j]] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+	won[albums[j]] = false;
 }
 
-for (let i = 0; i < 300; i++){
-	move(Math.floor(Math.random()*9));
+for (let j = 0; j < albums.length; j++){
+	for (let i = 0; i < 300; i++){
+		move(Math.floor(Math.random()*9), albums[j]);
+	}
 }
 
-function move(k){
+function move(k, album){
+	if (won[album]){
+		return;
+	}
 	if (k % dims != dims-1){
-		if (grid[k+1] == dims*dims-1){
-			grid[k+1] = grid[k];
-			grid[k] = dims*dims-1;
+		if (grids[album][k+1] == dims*dims-1){
+			grids[album][k+1] = grids[album][k];
+			grids[album][k] = dims*dims-1;
 		}
 	}
 	if (k % dims != 0){
-		if (grid[k-1] == dims*dims-1){
-			grid[k-1] = grid[k];
-			grid[k] = dims*dims-1;
+		if (grids[album][k-1] == dims*dims-1){
+			grids[album][k-1] = grids[album][k];
+			grids[album][k] = dims*dims-1;
 		}
 	}
 	if (k >= dims){
-		if (grid[k - dims] == dims*dims-1){
-			grid[k-dims] = grid[k];
-			grid[k] = dims*dims-1;
+		if (grids[album][k - dims] == dims*dims-1){
+			grids[album][k-dims] = grids[album][k];
+			grids[album][k] = dims*dims-1;
 		}
 
 	}
 	if (k < dims*dims - dims){
-		if (grid[k + dims] == dims*dims-1){
-			grid[k+dims] = grid[k];
-			grid[k] = dims*dims-1;
+		if (grids[album][k + dims] == dims*dims-1){
+			grids[album][k+dims] = grids[album][k];
+			grids[album][k] = dims*dims-1;
 		}
 
 	}
-	refresh("yeezus");
+	refresh(album);
+}
+
+function checkWon(album){
+	isWon = true;
+	for (let i = 0; i < grids[album].length; i++){
+		if (i != grids[album][i]){
+			isWon = false;
+		}
+	}
+	won[album] = isWon;
 }
 
 function refresh(album){
+	let isAlbum = (element) => element == album;
+	let cells = document.getElementsByClassName(`cell_${album_cells[albums.findIndex(isAlbum)]}`);
 	for (let i = 0; i < cells.length; i++){
-		cells[i].innerHTML = `<img src="${album}${grid[i]}.png" width=100px, height=100px>`
+		cells[i].innerHTML = `<img src="./${album}/${grids[album][i]}.png" width=100px, height=100px>`
 	}
 }
