862e0103d04eb355314e84a83f1a0743a80dfe91
jamdays <venuedollar@protonmail.com>
2024-07-05 21:46:42 -0700

    cat trap and fixed aquemini at eight puzzle

--- CatTrap/app.js
+++ CatTrap/app.js
@@ -1,7 +1,7 @@
 let rows = document.getElementsByClassName("row");
 for (let r = 0; r < rows.length; r++){
 	for (let c = 0; c < rows.length; c++){
-		rows[r].innerHTML += (r == 5 && c == 5) ? `<div class="cell" id="(${r},${c})"><div id="cat"></div></div>` : `<div class="cell" id="(${r},${c})"></div>`;
+		rows[r].innerHTML += (r == 5 && c == 5) ? `<div class="cell" id="(${r},${c})"><div id="cat"><div class="forehead"></div>&#9673 _ &#9673<br/> </div></div>` : `<div class="cell" id="(${r},${c})"></div>`;
 	}
 	if (r % 2 == 0){
 		rows[r].style.width = "600px";
