2c27f682cbbd7a74a94804c6f154e247d38f4ff7
jamdays <venuedollar@protonmail.com>
2024-12-10 00:09:12 -0500

    worked on channels

--- BluRay/app.js
+++ BluRay/app.js
@@ -1,5 +1,6 @@
 document.querySelector(".cdplayer").addEventListener("click", function(){cdplay();});
-
+let channels = ["cyberchase.mp4", "fetch.mp4"];
+let currchannel = 0;
 
 function cdplay(){
 	if (sessionStorage.getItem("hasCD") == "true"){
@@ -7,4 +8,39 @@ function cdplay(){
 	}
 }
 
+function changechannel(i){
+	currchannel = (currchannel + i)% channels.length
+	document.querySelector("video").src = channels[currchannel];
+}
+
+let types = ["basket1", "basket2", "basket3", "remote"];
+document.querySelector("#tvbaskethandle1").addEventListener("click", function(){popup("basket1");});
+document.querySelector("#tvbaskethandle2").addEventListener("click", function(){popup("basket2")});
+document.querySelector("#tvbaskethandle3").addEventListener("click", function(){popup("basket3")});
+document.querySelector(".remote").addEventListener("click", function(){popup("remote")});
+let inner = {"handle1": "", "handle2": "", "handle3": '', "remote": ''};
+
+inner["remote"] = "<div class='remotecontrol'><div id='chup'>\u25B3</div><div>ch</div><div id='chdn'>\u25BD</div></div>"; 
+
+
+let body = document.querySelector("body");
+function popup(type){
+	for (let i = 0; i < types.length; i++){
+		if (document.querySelector(`#${types[i]}popup`)){
+			return;
+		}
+	}
+	if (!document.querySelector(`#${type}popup`)){
+		body.innerHTML += `<div id="${type}up"class="popup" style="top:${screen.height/2 + Math.random()*50 + 50}px; left:${screen.width/2 + Math.random()*50 -225}px;"><div id="${type}x"class="x">X</div><div id="${type}popup">${inner[type]}</div></div>`;
+		document.querySelector(`#${type}x`).addEventListener("click", function(){document.querySelector(`#${type}up`).remove();});
+		if (type == "remote"){
+			document.querySelector("#chup").addEventListener("click", function(){changechannel(1);});
+			document.querySelector("#chdn").addEventListener("click", function(){changechannel(1);});
+		}
+	}
+	document.querySelector("#tvbaskethandle1").addEventListener("click", function(){popup("handle1");});
+	document.querySelector("#tvbaskethandle2").addEventListener("click", function(){popup("handle2")});
+	document.querySelector("#tvbaskethandle3").addEventListener("click", function(){popup("handle3")});
+	document.querySelector(".remote").addEventListener("click", function(){popup("remote")});
+}
 
