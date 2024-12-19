260b44bced968308aefdb0a479f5ed43f32d06a5
jamdays <venuedollar@protonmail.com>
2024-12-19 00:11:34 -0500

    finished up and fixed some bugs

--- Music/aquemini.js
+++ Music/aquemini.js
@@ -1,21 +1,31 @@
 const NOTES = [
-["On sight", 
-"First time I listened to the album I didn't like it, but it just gets better every time"],
-["Bound 2", 
-"Yeah Bound 2 falling in love, I mean true, but that's definitely not what this song is about"],
-["I am a God", 
-'"Would it be better if I had a song that said I am a nigga?" I mean thats what kanye says about the song but idk'],
-["Im In It", 'Kanye thinks he is a rap-lic priest'],
-["Bound 2 2", "Rip kanye and kim fr at least they got divorced to live happier lives instead of ---- and ---- who just decided to suffer instead"],
-["Blood on the leaves", "Yeah I lowkey wrote way too daring stuff on some other reviews, but this one is not about to get that blessing"]
+["Synthesizer", 
+"Moral of the story: Lapdance on your laptop while your laptop's in your lap. Onlyfans hard predict from this album. If you wanna synthisize I empathize. Individualism or something deep."],
+["Aquemnini", 
+"My mind warps and bends floats the wind count to ten " +
+"Meet the twin Andre Ben', welcome to the lion's den " +
+"Original skin, many men comprehend " +
+"I extend myself, so you go out and tell a friend " +
+"Sin all depends on what you believin' in " +
+"Faith is what you make it, that's the hardest shit since MC Ren " +
+"Alien can blend right on in wit' yo' kin " + 
+"Look again 'cause I swear I spot one every now and then "], 
+["Skew it on the Bar B", 
+'Old school player to new school fools. Basically Skibidi toilet summed up in a sentence, jk skibidi toilet is lowkey good'],
+["Chonkyfire", 
+"I got something to say"],
+["Yall Scared", 
+"Its not a problem until it affects whites"],
+["Liberation", 
+"Never gonna be free from whites no matter what you think. I mean not the point of the song but whatever. The point is actually way better. But idc honestly. Tired of listening to white ppls trash."]
 ]
 
 const QUOTES = [
-"Memories don't live like people do <br> they always rmember you",
-"I keep it 300, like the Romans <br> 300 bitches where the Trojans?",
-"Uh, they be ballin in the D League <br> Uh, I be speaking swaghili",
-"They prolly all in the Hamptons <br> Braggin' bout what they made <br> Fuck you and your hampton house <br> I'll fuck your hampton spouse",
-"I know that we the new slaves <br> y'all niggas can't fuck with me"
+"Can't worry 'bout what another nigga think <br> Now that's liberation and, baby, I want it",
+"The common denominator, the nigga numerator <br> Never know who the hater, niggas cater to your ego <br> I'm sorry like Atari whose the cousin to Coleco <br> Vision caught a RICO",
+"Hold on be strong",
+"So sleepy, playin' safe in cyberspace",
+"Digital good time, digital good time"
 ]
 
 let k = Math.random()
@@ -35,17 +45,14 @@ k = 0;
 for (let i = 0; i < notes.length; i++){
 	let prev_k = k;
 	let note = notes[i];
-	if (Math.random() > .8){
-		if (Math.random() > .5){
-			k++;
-		}
+	if (Math.random() > .9){
 		k++;
 	}
 	let rando = Math.random();
 	console.log(screen.height);
 	notes[i].style.top = Math.floor(Math.random()*50 + screen.height/2) + 'px';
 	notes[i].style.left = Math.floor(Math.random()*50 + (screen.width/5)*(i+1))+ 'px';
-	notes[i].innerHTML = `<h3 class="whiteboardtext">${NOTES[prev_k + Math.floor(rando*(k-prev_k))][0]}</h3><b>${NOTES[prev_k + Math.floor(rando*(k-prev_k))][1]}</b>`;
+	notes[i].innerHTML = `<h3 class="whiteboardtext">${NOTES[prev_k][0]}</h3><b>${NOTES[prev_k][1]}</b>`;
 	notes[i].addEventListener("mousedown", function(){dragDrop(note);});
 	k++;
 }
