2ac760c6f43fdcc1b6a48d5dc446f8c41a27b627
jamdays <venuedollar@protonmail.com>
2025-01-07 13:39:52 -0500

    worked on dots and boxes

---
+++ DotsAndBoxes/app.js
@@ -0,0 +1,27 @@
+
+
+function clicked(id){
+	let elem = document.querySelector("#" + id)
+	if (elem.style.backgroundColor == "#757575"){
+		return;
+	}
+	let intid = parseInt(id);
+	if(intid%10 < 3){
+		if (id > 3) {
+		}
+		if (id < 29) {
+
+		}
+	}
+}
+
+let vlines = document.getElementsByClassName("vline");
+let hlines = document.getElementsByClassName("hline");
+
+for (let i = 0; i < hlines.length; i++){
+
+}
+
+for (let i = 0; i < vlines.length; i++){
+
+}
