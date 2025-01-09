b05efe405c2770a2a065b6808ee5bc1ac36561f4
jamdays <venuedollar@protonmail.com>
2025-01-08 20:32:42 -0500

    dots and boxes more

--- DotsAndBoxes/app.js
+++ DotsAndBoxes/app.js
@@ -3,27 +3,32 @@ function claim(){
 }
 
 function clicked(id){
-	let elem = document.querySelector("#" + id)
+	let elem = document.querySelector("#" + id);
 	if (elem.style.backgroundColor == "#757575"){
 		return;
 	}
-	let intid = parseInt(id);
+	let intid = parseInt(id.substring(1));
 	if(intid%10 < 3){
-		if (id > 3) {
+		if (intid > 3) {
+
 		}
-		if (id < 29) {
+		if (intid < 29) {
 
 		}
 	}
+	elem.style.backgroundColor = "#757575";
 }
 
 let vlines = document.getElementsByClassName("vline");
 let hlines = document.getElementsByClassName("hline");
 
 for (let i = 0; i < hlines.length; i++){
-
+	let idstr = hlines[i].id;
+	hlines[i].addEventListener("click", function(){clicked(idstr)});
 }
 
 for (let i = 0; i < vlines.length; i++){
+	let idstr = vlines[i].id;
+	vlines[i].addEventListener("click", function(){clicked(idstr)});
 
 }
