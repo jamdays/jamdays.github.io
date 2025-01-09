8ea480534181161747c0698d280617c1fd0607d9
jamdays <venuedollar@protonmail.com>
2025-01-08 21:09:31 -0500

    dots and boxes not working but x and o look nice

--- DotsAndBoxes/app.js
+++ DotsAndBoxes/app.js
@@ -1,4 +1,17 @@
-function claim(){
+let turn = true;
+
+function tryclaim(n){
+	let sub = 4 + (n%10 - 4)/2
+	let add = 6 - (n%10 - 4)/2
+	console.log((n-1) + "");
+	if (document.querySelector("#" + (n-1).toString()) &&
+		document.querySelector("#" + (n+1).toString()) &&
+		document.querySelector("#" + (n-sub).toString()) &&
+		document.querySelector("#" + (n+add).toString())){
+		turn = !turn;
+		let entry = turn ? "o" : "x";
+		document.querySelector("#a" + n.toString()).innerHTML += `<div class="{entry}"></div>`;
+	}
 
 }
 
@@ -10,12 +23,15 @@ function clicked(id){
 	let intid = parseInt(id.substring(1));
 	if(intid%10 < 3){
 		if (intid > 3) {
-
+			//TODO THIS GUY PASS IT IN ALSO DEAL WITH DOING TWO AT ONCE
+			tryclaim();
 		}
 		if (intid < 29) {
-
+			//TODO THIS GUY PASS IT IN ALSO DEAL WITH DOING TWO AT ONCE
+			tryclaim();
 		}
 	}
+	turn = !turn;
 	elem.style.backgroundColor = "#757575";
 }
 
